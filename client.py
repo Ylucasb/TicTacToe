301673f103b5e859b006d91ef87a15b0ead9c599
bilucas <lucas.bigot@ynov.com>
2024-04-10 14:55:18 +0200

    :sparkles: | try to merge

--- client.py
+++ client.py
@@ -1,43 +1,74 @@
 import socket
 import threading
 
-def receive_from_server(server_socket):
-    while True:
-        try:
-            data = server_socket.recv(1024)
-            if not data:
-                print("La connexion avec le serveur a été fermée.")
-                break
-            print("\nMessage du serveur:", data.decode())
-            print("Entrez votre réponse (ou 'exit' pour quitter) : ")
-        except Exception as e:
-            print("Une erreur s'est produite lors de la réception des données du serveur:", e)
-            break
-
-def main():
+class client:
+    table = list()
     server_ip = '127.0.0.1' 
     server_port = 12345 
-
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    try:
-        client_socket.connect((server_ip, server_port))
-        print("Connexion établie avec le serveur.")
-        server_thread = threading.Thread(target=receive_from_server, args=(client_socket,))
-        server_thread.start()
-
+    client_socket = any
+    responseFromServer = ""
+    win = False
+    def __init__(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            self.client_socket.connect((self.server_ip, self.server_port))
+            print("Connexion établie avec le serveur.")
+            server_thread = threading.Thread(target=self.receive_from_server, args=(self.client_socket,))
+            server_thread.start()
+        except Exception as e:
+            print("Une erreur s'est produite lors de la connexion au serveur:", e)
+            
+    def receive_from_server(self, server_socket):
         while True:
-            message = input("Entrez votre message ('exit' pour quitter) : ")
-            client_socket.sendall(message.encode())
-            if message.lower() == 'exit':
-                break
-
-    except Exception as e:
-        print("Une erreur s'est produite lors de la connexion au serveur:", e)
-
-    finally:
-        if client_socket:
-            client_socket.close()
-
+            try:
+                data = server_socket.recv(1024)
+                if not data:
+                    print("La connexion avec le serveur a été fermée.")
+                    return 
+                self.responseFromServer = data.decode()
+                print("receive :", self.responseFromServer)
+                if self.responseFromServer != "":
+                    self.restoreTable(self.responseFromServer[:9])
+                    print(self.table)
+                    message = self.responseFromServer[9:]
+                    print(message)
+                    if message == "play":
+                        print(f"C'est à vous de jouer")
+                        selectedLine = int(input("Donner la ligne sélectionnée (0,1,2): "))
+                        selectedRow = int(input("Donner la colonne sélectionnée (0,1,2): "))
+                        if self.isValid(selectedLine, selectedRow):  
+                            message = str(selectedLine) + str(selectedRow)
+                            print("message",message)
+                            self.client_socket.sendall(message.encode())
+                        else:
+                            while not self.isValid(selectedLine, selectedRow):
+                                print(f"C'est à vous de jouer input invalide")
+                                selectedLine = int(input("Donner la ligne sélectionnée (0,1,2): "))
+                                selectedRow = int(input("Donner la colonne sélectionnée (0,1,2): "))
+                            message = str(selectedLine) + str(selectedRow)
+                            print("message",message)
+                            self.client_socket.sendall(message.encode())
+                    elif message == "loose":
+                        print("Vous avez perdu")
+                    else:
+                        print("Vous avez gagné")
+            except Exception as e:
+                print("Une erreur s'est produite lors de la réception des données du serveur:", e)
+                return
+            
+    def isValid(self, selectedLine, selectedRow):
+        return selectedLine>=0 and selectedLine<=3 and selectedRow>=0 and selectedRow<=3 and self.table[selectedLine][selectedRow] =="."
+    
+    def restoreTable(self, encodedTable):
+        line = list()
+        i = 0
+        for element in encodedTable:
+            i +=1
+            line.append(element)
+            if i == 3:
+                self.table.append(line)
+                line = []
+                i = 0
+                
 if __name__ == "__main__":
-    main()
+    client()
--- server.py
+++ server.py
@@ -1,27 +1,37 @@
 import random
+import socket
+import threading
+import time
 class Server:
+    server_ip = '127.0.0.1' 
+    server_port = 12345 
     table = [["."]*3 for _ in range(3)]
     whoPlay = 0
     playerWhoWin = 0
+    server_socket = any
+    client_socket = any
+    inputFromClient = ""
     def __init__(self):
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.whoPlay = random.randint(0,1)
-        self.game()
-    def game(self):
-        while self.isNotFinish():
-            self.whoPlay = 1 if self.whoPlay == 0 else 0
-            print(f"C'est au joueur n°{self.whoPlay} de jouer")
-            selectedLine = int(input("Donner la ligne sélectionnée (0,1,2): "))
-            selectedRow = int(input("Donner la colonne sélectionnée (0,1,2): "))
-            if self.isValid(selectedLine, selectedRow):
-                self.table[selectedLine][selectedRow] = "0" if self.whoPlay == 0 else "X"
-            else:
-                while not self.isValid(selectedLine, selectedRow):
-                    print(f"Numéro invalide")
-                    selectedLine = int(input("Donner la ligne sélectionnée (0,1,2): "))
-                    selectedRow = int(input("Donner la colonne sélectionnée (0,1,2): "))
-                self.table[selectedLine][selectedRow] = "0" if self.whoPlay == 0 else "X"
-            print(self.table)
-        print(f"La partie est finie le joueur {self.playerWhoWin} a gagné")
+        try:
+            server_socket.bind((self.server_ip, self.server_port))
+            server_socket.listen(5)
+            print("Le serveur écoute sur {}:{}".format(self.server_ip, self.server_port))
+            self.client_socket, client_address = server_socket.accept()
+            print("Connexion entrante de:", client_address)
+            self.game()
+        except Exception as e:
+            print("Une erreur s'est produite lors de l'exécution du serveur:", e)
+            
+    def listening(self, client_socket):
+        data = client_socket.recv(1024)
+        if not data:
+            print("La connexion avec le client a été fermée.")
+            return
+        self.inputFromClient = data.decode()
+        print("receive : ",self.inputFromClient)
+        
     def isNotFinish(self):
         for i in range (0,3):
             if all(element == self.table[i][0] != "." for element in self.table[i]):
@@ -40,10 +50,42 @@ class Server:
             if self.table[0][2] == self.table[1][1] == self.table[2][0] != ".":
                 self.playerWhoWin = 0 if self.table[i][0] == "0" else 1
                 return False
-            
         return True
+    
     def isValid(self, selectedLine, selectedRow):
         return selectedLine>=0 and selectedLine<=3 and selectedRow>=0 and selectedRow<=3 and self.table[selectedLine][selectedRow] =="."
 
+    def game(self):
+        while self.isNotFinish():
+            self.whoPlay = 1 if self.whoPlay == 0 else 0
+            if self.whoPlay == 0:
+                print(f"C'est au joueur {self.whoPlay} de jouer")
+                response = ''.join(''.join(str(element) for element in line) for line in self.table) + "play"
+                self.client_socket.sendall(response.encode())
+                while self.inputFromClient == "":
+                    client_thread = threading.Thread(target=self.listening, args=(self.client_socket,))
+                    client_thread.start()
+                self.table[int(self.inputFromClient[0])][int(self.inputFromClient[1])] = "0" if self.whoPlay == 0 else "X"
+            else:
+                print(f"C'est à vous de jouer")
+                selectedLine = int(input("Donner la ligne sélectionnée (0,1,2): "))
+                selectedRow = int(input("Donner la colonne sélectionnée (0,1,2): "))
+                if self.isValid(selectedLine, selectedRow):
+                    self.table[selectedLine][selectedRow] = "0" if self.whoPlay == 0 else "X"
+                else:
+                    while not self.isValid(selectedLine, selectedRow):
+                        print(f"C'est à vous de jouer input invalide")
+                        selectedLine = int(input("Donner la ligne sélectionnée (0,1,2): "))
+                        selectedRow = int(input("Donner la colonne sélectionnée (0,1,2): "))
+                    self.table[selectedLine][selectedRow] = "0" if self.whoPlay == 0 else "X"
+            print(self.table)
+            
+        print(f"La partie est finie le joueur {self.playerWhoWin} a gagné")
+        if self.playerWhoWin == 0:
+            response = ''.join(''.join(str(element) for element in line) for line in self.table)  + "win"
+        else:
+            response = ''.join(''.join(str(element) for element in line) for line in self.table)  + "loose"
+        self.client_socket.sendall(response.encode())
+        
 if __name__ == "__main__":
-    server = Server()
+    Server()
